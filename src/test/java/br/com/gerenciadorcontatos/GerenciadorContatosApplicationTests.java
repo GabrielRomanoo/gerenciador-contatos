092de378905a1247cc9d704cba037521b066ee8f
Gabriel Romano <gabriel.romano.3194@hotmail.com>
2023-07-02 00:15:03 -0300

    commit inicial

---
+++ src/test/java/br/com/gerenciadorcontatos/GerenciadorContatosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gerenciadorcontatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorContatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
